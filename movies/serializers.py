72697283dacf5a4fcfba780e74a172b74988645a
David Schurer <david.schurer@yahoo.com>
2024-07-18 16:23:23 -0700

    update

--- movies/serializers.py
+++ movies/serializers.py
@@ -2,9 +2,11 @@ from rest_framework import serializers
 from .models import Movie, Review
 
 class ReviewSerializer(serializers.ModelSerializer):
+    movie_title = serializers.ReadOnlyField(source='movie.title')
+
     class Meta:
         model = Review
-        fields = ['id', 'movie', 'rating', 'review']
+        fields = ['id', 'movie', 'movie_title', 'rating', 'review']
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
